174992c2a1c7ba70455afc4edf1abdea7a472bb7
Ignacio Llamedo <ignaciollamedo@hotmail.com>
2026-04-30 22:55:53 -0300

    crear

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+import config from './config.js'
+import express from 'express'
+
+const app = express()
+
+app.listen(config.port, () => {
+    console.log(`Listening in port ${config.port}`)
+})
+
 console.log("aaaaaa")
