483c8c55601ebe8b8a17a0be9c72c117600bc818
Facundo Ortega <fac.2002ortega@gmail.com>
2026-05-18 13:47:37 -0300

    M manual

---
+++ src/Front/Static/Access/twoFactorAuthentication.js
@@ -0,0 +1,45 @@
+const parameteresURL = new URLSearchParams(window.location.search);
+const verificationErrorMsg = document.getElementById("verificationError");
+
+const email = parametrosURL.get('mail');
+
+document.getElementById("reenviar-codigo").addEventListener("click", async (event) => {
+    const res = await fetch("/api/..", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: email
+    });
+});
+
+document.getElementById("two-factor-authenticator-form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const codigo = event.target.codigo.value;
+
+    const data = {
+        mail: email,
+        codigo: codigo,
+    }
+
+    const dataString = JSON.stringify(data);
+
+    const res = await fetch("/api/authenticate", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: dataString
+    });
+
+    const resData = await res.json();
+
+    if(resData.success)
+        window.location.href = resData.redirect;
+    else 
+    {
+        verificationErrorMsg.textContent = resData.message;
+        verificationErrorMsg.classList.remove("hidden");
+    } 
+});
