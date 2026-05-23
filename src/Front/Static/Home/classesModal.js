91b707368767cf3ca31c7a8e97ba49b128f1159b
lean-ll <leandro_llorente@outlook.com>
2026-05-22 23:02:30 -0300

    (parte 2) html de home+perfil+tabla+actividades unificados y convertidos a .ejs, y agregadas navbars de roles

--- src/Front/Static/Home/classesModal.js
+++ src/Front/Static/Home/classesModal.js
@@ -2,18 +2,20 @@ const classesLink = document.getElementById("classesLink");
 const tablaModal = document.getElementById("tablaModal");
 const closeModal = document.getElementById("closeModal");
 
-classesLink.addEventListener("click", (e) => {
+if (classesLink && tablaModal && closeModal) {
+  classesLink.addEventListener("click", (e) => {
     e.preventDefault(); // evita que navegue
     tablaModal.style.display = "flex"; // muestra el modal
-});
+  });
 
-closeModal.addEventListener("click", () => {
+  closeModal.addEventListener("click", () => {
     tablaModal.style.display = "none"; // cierra el modal
-});
+  });
 
-// Cerrar si se hace clic fuera del contenido
-window.addEventListener("click", (e) => {
+  // Cerrar si se hace clic fuera del contenido
+  window.addEventListener("click", (e) => {
     if (e.target === tablaModal) {
-    tablaModal.style.display = "none";
+      tablaModal.style.display = "none";
     }
-});
+  });
+}
