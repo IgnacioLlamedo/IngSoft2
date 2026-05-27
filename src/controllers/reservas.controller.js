bff50d31597412f64da589b9b70b92e5ec958a7c
FacuoOrtega2026 <facundo.ortega180037@alumnos.info.unlp.edu.ar>
2026-05-27 10:24:37 -0300

    Limpiar DB

--- src/controllers/reservas.controller.js
+++ src/controllers/reservas.controller.js
@@ -5,6 +5,115 @@ import { salaDao } from "../daos/index.js";
 import { profesorDao } from "../daos/index.js";
 
 
+/* export async function getMyReservations(req, res) {
+    try {
+
+        if(!req.session.user) {
+            return res.json({
+                success: false,
+                message: "usuario no autenticado."
+            });
+        }
+
+        const idUsuario = req.session.user.id;
+        console.log("Id de usuario desde reservas.controller: ");
+        console.log(idUsuario);
+
+        //Obtengo las reservas de clases únicas y mensuales del usuario
+        const reservasUnicas = await reservaDao.readManyUnica({
+            idUsuario,
+            cancelada: false
+        });
+
+        const reservasMensuales = await reservaDao.readManyMensual({
+            idUsuario,
+            cancelada: false
+        });
+
+        //Desarmar los arrays obtenidos (se unen las unicas y mensuales)
+        /**
+         * Queda algo como:
+         * [
+            reservaUnica1,
+            reservaUnica2,
+            reservaMensual1
+            ]
+        
+        const reservas = [
+            ...reservasUnicas,
+            ...reservasMensuales
+        ];
+        console.log("Las reservas unicas y mensuales del usuario son: ")
+        console.log(reservas);
+
+        /* Esto estaria bueno modificarlo porque se traen todas las clases,
+        actividades, salas y profesores.
+        Y a partir de ahí se filtran segun las reservas que tiene el usuario.
+        Según consulté con IA, se puede hacer con populate (ni me gaste xd)
+        const clases = await claseGeneralDao.readMany({});
+        const actividades = await actividadDao.readMany({});
+        const salas = await salaDao.readMany({});
+        const profesores = await profesorDao.readMany({});
+
+        const reservasTotal = reservas.map(reserva => {
+
+            //SQL joins intensifies :v
+            const clase = clases.find(
+                c => c._id === reserva.idClase
+            );
+            console.log("Una clase dentro de reserva es: ");
+            console.log(clase); //si retorna undefined es porque el idClase
+            //en reserva estaba hardcodeado y no existe una clase con ese id.
+
+            /* Esto realmente es por temas de testeo,
+            ya que en las reservas, las id de clase están hardcodeadas
+            y al buscar la clase, devuelve undefined.
+            Al eliminar el hard, se puede quitar
+            if (!clase) {
+                return null;
+            }
+
+            const actividad = actividades.find(
+                a => a._id === clase.idActividad
+            );
+
+            const sala = salas.find(
+                s => s._id === clase.idSala
+            );
+
+            const profesor = profesores.find(
+                p => p._id === clase.idProfesor
+            );
+
+            console.log("Información de las reservas del usuario: ");
+            console.log(clase, actividad, sala, profesor);
+
+            return {
+                reserva,
+                clase,
+                actividad,
+                sala,
+                profesor
+            };
+            
+        }).filter(Boolean); //Filtra los null
+
+        res.json({
+            success: true,
+            reservas: reservasTotal
+        });
+
+    }
+    catch(error) {
+        console.log(error);
+
+        res.json({
+            success: false,
+            message: error.message
+        });
+    }
+} */
+
 export async function getMyReservations(req, res) {
     try {
 
@@ -50,7 +159,9 @@ export async function getMyReservations(req, res) {
         actividades, salas y profesores.
         Y a partir de ahí se filtran segun las reservas que tiene el usuario.
         Según consulté con IA, se puede hacer con populate (ni me gaste xd) */
-        const clases = await claseGeneralDao.readMany({});
+
+        //Sin populate
+        /* const clases = await claseGeneralDao.readMany({});
         const actividades = await actividadDao.readMany({});
         const salas = await salaDao.readMany({});
         const profesores = await profesorDao.readMany({});
@@ -65,10 +176,7 @@ export async function getMyReservations(req, res) {
             console.log(clase); //si retorna undefined es porque el idClase
             //en reserva estaba hardcodeado y no existe una clase con ese id.
 
-            /* Esto realmente es por temas de testeo,
-            ya que en las reservas, las id de clase están hardcodeadas
-            y al buscar la clase, devuelve undefined.
-            Al eliminar el hard, se puede quitar */ 
+            //Esto realmente es por temas de testeo, ya que en las reservas, las id de clase están hardcodeadas y al buscar la clase, devuelve undefined. Al eliminar el hard, se puede quitar 
             if (!clase) {
                 return null;
             }
@@ -96,8 +204,17 @@ export async function getMyReservations(req, res) {
                 profesor
             };
             
-        }).filter(Boolean); //Filtra los null
+        }).filter(Boolean); //Filtra los null */
+        
+        const reservasUnicasTotal = await reservaDao.populateUnica({ idUsuario: idUsuario })
+        const reservasMensualesTotal = await reservaDao.populateMensual({ idUsuario: idUsuario })
+        let reservasTotal = []
+        reservasTotal = reservasTotal.concat(reservasUnicasTotal)
+        reservasTotal = reservasTotal.concat(reservasMensualesTotal)
 
+        console.log("Las reservas TOTALES del usuario son: ")
+        console.log(reservasTotal);
+        
         res.json({
             success: true,
             reservas: reservasTotal
