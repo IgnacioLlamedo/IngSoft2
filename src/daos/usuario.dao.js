a34103fcd176fee06bf088c3b4b3c3e6a6d8b136
lean-ll <leandro_llorente@outlook.com>
2026-06-03 21:24:00 -0300

    hechos más descriptivos los logs de usuario.dao

--- src/daos/usuario.dao.js
+++ src/daos/usuario.dao.js
@@ -2,44 +2,47 @@ import { Usuario } from "../models/usuario.mongoose.js";
 
 export class usuarioDao {
     async create(datos){
+        console.log(`Ejecutando usuarioDao.create(datos).`);
+        console.log(`datos: ${JSON.stringify(datos)}`);
         return await Usuario.create(datos)
-        console.log(datos)
     }
     async readOne(query){
-        const usuario = await Usuario.findOne(query).lean()
+        const usuario = await Usuario.findOne(query).lean();
         if(!usuario){
             //provisional, desarrollar luego
-            console.log("error con: ")
-            console.log(query);
+            console.log(`ERROR? al ejecutar usuarioDao.readOne(query)`);
+            console.log(`query: ${JSON.stringify(query)}`);
         }
-        return usuario
+        return usuario;
     }
     async readMany(query){
-        return await Usuario.find(query).lean()
+        return await Usuario.find(query).lean();
     }
     async updateOne(query, datos){
-        const updated = await Usuario.findOneAndUpdate({ mail: query }, datos, { returnDocument: 'after' }).lean()
+        const updated = await Usuario.findOneAndUpdate({ mail: query }, datos, { returnDocument: 'after' }).lean();
         if(!updated){
-            //provisional, desarrollar luego
-            console.log("error")
+            console.log(`ERROR? al ejecutar usuarioDao.updateOne(query, datos)`);
+            console.log(`query: ${JSON.stringify(query)} / datos: ${JSON.stringify(datos)}`);
         }
-        return updated
+        return updated;
     }
     async deleteOne(query){
-        const deleted = await Usuario.findOneAndDelete({ mail: query }).lean()
+        const deleted = await Usuario.findOneAndDelete({ mail: query }).lean();
         if(!deleted){
             //provisional, desarrollar luego
-            console.log("error")
+            console.log(`ERROR? al ejecutar usuarioDao.deleteOne(query)`);
+            console.log(`query: ${JSON.stringify(query)}`);
         }
-        return deleted
+        return deleted;
     }
     async deleteMany(query){
-        const deleted = await Usuario.deleteMany(query).lean()
+        const deleted = await Usuario.deleteMany(query).lean();
         if(!deleted){
             //provisional, desarrollar luego
-            console.log("error")
+            console.log(`ERROR? al ejecutar usuarioDao.deleteMany(query)`);
+            console.log(`query: ${JSON.stringify(query)}`);
         }
-        return deleted
+        return deleted;
     }
 }
 
