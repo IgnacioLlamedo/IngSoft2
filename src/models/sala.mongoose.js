b1a26b0b10886da48f4e2a295d39d48f15042b71
lean-ll <leandro_llorente@outlook.com>
2026-07-01 16:09:17 -0300

    modificados daos de salas y clases específicas para indicar el estado

--- src/models/sala.mongoose.js
+++ src/models/sala.mongoose.js
@@ -6,7 +6,10 @@ const collection = 'salas'
 const salaSchema = new Schema({
     _id: { type: String, default: randomUUID },
     nombre: { type: String, required: true },
-    limiteSala: { type: Number, required: true }
+    limiteSala: { type: Number, required: true },
+    estado: { type: String, enum: ['habilitada', 'inhabilitada', 'borrada'], default: 'habilitada' },
+    motivoEstado: { type: String, default: 'Sin motivo especificado' },
+    fechaEstado: { type: Date, required: true },
 }, {
     strict: 'throw',
     versionKey: false
